d9ef3f5c467225b67ee3046e79c917f70cde8cf1
azertypow <nico@villa1203.ch>
2025-07-02 16:24:21 +0200

    chore: test

--- server/api/test.ts
+++ server/api/test.ts
@@ -5,24 +5,24 @@ export default defineEventHandler(async (event) => {// console.log('data', data)
 
     const authHeader = Buffer.from(`${email}:${password}`).toString('base64')
 
-    const dataApi = await $fetch('http://localhost:8000/api/query', {
-        lazy: true,
-        method: 'POST',
-        headers: {
-            'Authorization': `Basic ${authHeader}`
-        },
-        body: {
-            query: 'site().children',
-            select: {
-                'title' : true,
-                'slug' : true,
-                'contenu' : true,
-                'show_in_nav' : true,
-            }
-        }
-    })
-
-    console.log('dataApi', dataApi)
+    // const dataApi = await $fetch('http://localhost:8000/api/query', {
+    //     lazy: true,
+    //     method: 'POST',
+    //     headers: {
+    //         'Authorization': `Basic ${authHeader}`
+    //     },
+    //     body: {
+    //         query: 'site().children',
+    //         select: {
+    //             'title' : true,
+    //             'slug' : true,
+    //             'contenu' : true,
+    //             'show_in_nav' : true,
+    //         }
+    //     }
+    // })
+    //
+    // console.log('dataApi', dataApi)
 
     return {
         data: "test"
