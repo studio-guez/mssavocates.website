971793bafbb3bcdf2f885462f42b928fe5238608
azertypow <nico@villa1203.ch>
2026-03-02 12:30:03 +0100

    fix: config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
+
+  app: {
+    baseURL: '/',
+  },
+
+  runtimeConfig: {
+    secret_API_AUTH_EMAIL: process.env.API_AUTH_EMAIL,
+    secret_API_AUTH_PASSWORD: process.env.API_AUTH_PASSWORD,
+    public: {
+      apiUrl: process.env.NUXT_API_URL,
+    }
+  },
+
   devServer: {
     port: 3001,
   },
